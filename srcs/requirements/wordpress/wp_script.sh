eba8daaffb3ed1a3d23a57db5a4b87e9e349bafb
Maria CHEREL <mcherel-@student.42.fr>
2023-06-14 12:56:02 +0200

    atill all working

--- srcs/requirements/wordpress/wp_script.sh
+++ srcs/requirements/wordpress/wp_script.sh
@@ -5,7 +5,11 @@ while ! mariadb -h$DB_HOST -u$DB_ADMIN -p$DB_ADMIN_PASSWORD; do
     sllep 2
 done
 
-# if [ ! -f wp-config.php ]; then
-#     wp core download --allow-root
-#     wp config create --dbname="$DB_NAME" --dbuser="$DB_ADMIN" \
-#         --dbpass="$DB_ADMIN_PASSWORD" --dbhost="$DB_HOST" --allow-root
+if [ ! -f wp-config.php ]; then
+    wp core download --allow-root
+    wp config create --dbname="$DB_NAME" --dbuser="$DB_ADMIN" \
+        --dbpass="$DB_ADMIN_PASSWORD" --dbhost="$DB_HOST" --allow-root
+    
+    wp core install --url="$DOMAIN_NAME" --title "mcherel- inception" \
+        --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_MAIL" --skip-email --allow-root
